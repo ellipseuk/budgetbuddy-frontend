6bd646a77d6f97f6af37a1c93fc4d4b0349f5164
darthpenetratus <daniilgustenko@gmail.com>
2024-07-20 23:44:01 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/budget_buddy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.budget_buddy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BudgetBuddy',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BudgetBuddy'),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
