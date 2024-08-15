a4a56c2a52b79a8343869a1301ac947864b21c5e
Andrejs Jakunins <duckssupplier@gmail.com>
2024-08-15 17:47:43 +0300

    Folder renamed

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:budget_buddy/theme/theme.dart';
-import 'package:budget_buddy/router/routes.dart';
+import 'package:budget_buddy/routes/routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
